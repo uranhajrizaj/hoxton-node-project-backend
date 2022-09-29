083bb68725b27acf67cc3103ab877965c9c2a114
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-09-29 14:13:14 +0200

    updated

---
+++ prisma/migrations/20220928095538_not_required_image/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT
+);
+INSERT INTO "new_User" ("email", "id", "image", "name", "password", "surname") SELECT "email", "id", "image", "name", "password", "surname" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv'
+const socket = require("socket.io");
+
 dotenv.config()
 
 const app = express();
@@ -12,8 +14,9 @@ const SECRET= process.env.SECRET!
 app.use(cors());
 app.use(express.json());
 
-const port = 4000;
+const port = 4001;
 const prisma = new PrismaClient({ log: ["error", "info", "query", "warn"] });
+
 
 
 function getToken (id: number) {
@@ -21,13 +24,13 @@ function getToken (id: number) {
     expiresIn: '2 minutes'
   })
 }
-
+// {select:{friend1Id:true,friend2Id:true}
 async function getCurrentUser (token: string) {
   const decodedData = jwt.verify(token, SECRET)
   const user = await prisma.user.findUnique({
     // @ts-ignore
     where: { id: decodedData.id },
-    include: { following: true }
+    include: { following:{select:{friend2:{select:{name:true,surname:true,image:true}}}} }
   })
   return user
 }
@@ -44,7 +47,6 @@ app.post("/sign-up", async (req, res) => {
           name: req.body.name,
           surname: req.body.surname,
           email: req.body.email,
-          image: req.body.image,
           password: bcrypt.hashSync(req.body.password),
         },
         include:{followedby:true,following:true}
@@ -60,7 +62,7 @@ app.post("/sign-up", async (req, res) => {
 app.post("/sign-in", async (req, res) => {
   const findUser = await prisma.user.findUnique({
     where: { email: req.body.email },
-    include:{following:true}
+    include: { following:{select:{friend2:{select:{name:true,surname:true,image:true}}}} }
   });
   if(findUser&& bcrypt.compareSync(req.body.password,findUser.password)){
     res.send({user:findUser, token:getToken(findUser.id)})
@@ -83,6 +85,28 @@ app.get('/validate', async (req, res) => {
 })
 
 
-app.listen(port, () => {
+const server=app.listen(port, () => {
   console.log(`Serveri is running on: http://localhost:${port}`);
 });
+
+
+const io=socket(server);
+
+//initializing the socket io connection 
+io.on("connection", (socket:any) => {
+  //for a new user joining the chat
+  socket.on("joinChat", async (friendEmail:string,useremail:string) => {
+  //user sending a message
+  const friend= await prisma.user.findUnique({where:{email:friendEmail}})
+  const user =await prisma.user.findUnique({where:{email:useremail}})
+  socket.on("chat", (text:any) => {
+    //gets the room user and the message sent
+    io.to(user).emit("message", {
+      userId: user?.id,
+      username: user?.email,
+      text: text,
+    });
+  })
+})
+})
+
